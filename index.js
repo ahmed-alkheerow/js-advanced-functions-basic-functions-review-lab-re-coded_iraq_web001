08c3d6c9cefe05f7ac50d2fdc297bc4c21082b06
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-06-10 13:44:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function mondayWork(activity="go to the office"){
   return`This Monday, I will ${activity}.`;
 };
 mondayWork();
+
+function wrapAdjective(whatever ="*" ) {
+  return function innerFunction(parameter1 ="special") {
+    return `You are ${parameter1}`
+
+  }
+}
